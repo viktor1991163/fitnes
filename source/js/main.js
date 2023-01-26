453cd24db9263149e78f11628c80dc6498471805
viktor <ignatev-viktor@yandex.ru>
2023-01-27 07:10:09 +0800

    второй коммит

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,11 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initPlay} from './modules/video';
+import {initTab} from './modules/tab';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
   initPlay();
+  initTab();
 });
---
+++ source/js/modules/tab.js
@@ -0,0 +1,37 @@
+const initTab = () => {
+
+  const tabLinks = document.querySelectorAll('.products__item-time');
+  const tabContent = document.querySelectorAll('[data-money]');
+
+
+  tabLinks.forEach((el) => {
+    el.addEventListener('click', openTabs);
+    el.addEventListener('keydown', openTabs);
+  });
+
+
+  function openTabs(el) {
+    if (el.code === 'Enter' || el.code === 'Space' || el.button === 0) {
+      const btnTarget = el.currentTarget;
+      const long = btnTarget.dataset.long;
+
+      tabLinks.forEach((e) => {
+        e.classList.remove('products__item-time--active');
+      });
+
+      tabContent.forEach((e) => {
+        e.classList.remove('active');
+      });
+
+      const tabContentActive = document.querySelectorAll(`[data-money='${long}']`);
+
+      tabContentActive.forEach((e) => {
+        e.classList.add('active');
+      });
+
+      btnTarget.classList.add('products__item-time--active');
+    }
+  }
+}
+
+export {initTab};
