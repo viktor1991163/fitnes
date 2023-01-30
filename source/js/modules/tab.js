7c80029e574f8bbedeb275e127cb099522cfc390
viktor <ignatev-viktor@yandex.ru>
2023-01-30 21:08:35 +0800

    четвертый коммит

--- source/js/modules/tab.js
+++ source/js/modules/tab.js
@@ -1,21 +1,21 @@
+const tabLinks = document.querySelectorAll('.products__item-time');
+
+
 const initTab = () => {
-
-  const tabLinks = document.querySelectorAll('.products__item-time');
-  const tabContent = document.querySelectorAll('[data-money]');
-
 
   tabLinks.forEach((el) => {
     el.addEventListener('click', openTabs);
     el.addEventListener('keydown', openTabs);
   });
 
-
   function openTabs(el) {
     if (el.code === 'Enter' || el.code === 'Space' || el.button === 0) {
+      const tabLinkss = document.querySelectorAll('.products__item-time');
+      const tabContent = document.querySelectorAll('[data-money]');
       const btnTarget = el.currentTarget;
       const long = btnTarget.dataset.long;
 
-      tabLinks.forEach((e) => {
+      tabLinkss.forEach((e) => {
         e.classList.remove('products__item-time--active');
       });
 
