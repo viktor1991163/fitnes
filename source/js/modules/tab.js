a20789e45646156c75543e76fcf6ffcf2b33c772
viktor <ignatev-viktor@yandex.ru>
2023-02-01 20:20:35 +0800

    пятый коммит

--- source/js/modules/tab.js
+++ source/js/modules/tab.js
@@ -1,37 +1,37 @@
-const tabLinks = document.querySelectorAll('.products__item-time');
-
-
 const initTab = () => {
+
+  const tabLinks = document.querySelectorAll('.products__item-time');
+  const tabContent = document.querySelectorAll('[data-money]');
+
 
   tabLinks.forEach((el) => {
     el.addEventListener('click', openTabs);
     el.addEventListener('keydown', openTabs);
   });
 
+
   function openTabs(el) {
     if (el.code === 'Enter' || el.code === 'Space' || el.button === 0) {
-      const tabLinkss = document.querySelectorAll('.products__item-time');
-      const tabContent = document.querySelectorAll('[data-money]');
       const btnTarget = el.currentTarget;
       const long = btnTarget.dataset.long;
 
-      tabLinkss.forEach((e) => {
+      tabLinks.forEach((e) => {
         e.classList.remove('products__item-time--active');
       });
 
       tabContent.forEach((e) => {
-        e.classList.remove('active');
+        e.classList.remove('products__list-price--active');
       });
 
       const tabContentActive = document.querySelectorAll(`[data-money='${long}']`);
 
       tabContentActive.forEach((e) => {
-        e.classList.add('active');
+        e.classList.add('products__list-price--active');
       });
 
       btnTarget.classList.add('products__item-time--active');
     }
   }
-}
+};
 
 export {initTab};
